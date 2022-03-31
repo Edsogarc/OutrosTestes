8b904adf2c458593ec7329b8fc4426bb71ef1319
Edsogarc <nosdesdhm@gmail.com>
2022-03-31 12:52:13 -0300

    Inverter um inteiro

---
+++ Exe12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// Inverter um inteiro
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número para ser invertido: ");
+        Int64 numero = Int64.Parse(Console.ReadLine());
+
+        Inverter(numero);
+
+
+        static void Inverter(Int64 n)
+        {
+            string valor = Convert.ToString(n);
+            string[] valores = new string[valor.Length];
+            for (int i = 0; i < valor.Length; i++)
+            {
+                valores[i] = valor[i] + "";
+                if (i == valor.Length - 1)
+                {
+                    Console.Write(valores[i] + " --> ");
+                    for (int j = 1; j <= valores.Length; j++)
+                    {
+                        Console.Write(valores[valores.Length - j]);
+                    }
+                }
+                else
+                {
+                    Console.Write(valores[i]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
