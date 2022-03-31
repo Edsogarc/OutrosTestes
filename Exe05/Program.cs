7765c78973bb0282e2b1ec03262a501500164616
Edsogarc <nosdesdhm@gmail.com>
2022-03-31 12:42:03 -0300

    Contar letras da última palavra

---
+++ Exe05/Program.cs
@@ -0,0 +1,27 @@
+﻿// Contar letras da última palavra
+class Program
+{
+    static void Main(string[] args)
+    {
+        string frase1 = "Na minha máquina funciona";
+        string frase2 = "Eu posso pular o teste";
+        string frase3 = "Isso não deve demorar";
+
+        ContarLetras(frase1);
+        ContarLetras(frase2);
+        ContarLetras(frase3);
+
+    }
+    static void ContarLetras(string frase)
+    { 
+        string[] palavra = frase.Split(' ');
+        int palavras = palavra.Length;
+        int i = 0;
+        while (i != palavras)
+        {
+            i++;
+        }
+        i--;
+        Console.WriteLine(palavra[i].Length);
+    }
+}
